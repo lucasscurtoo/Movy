a857fd59f0b4806d5d5c01bf73e6e30955394846
lucasscurtoo <lucascurtoo@gmail.com>
2021-08-12 15:51:00 -0300

    adding home, movies and promises to the API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from './pages/main';
@@ -5,6 +6,16 @@ import LogIn from './pages/logIn';
 import UserHome from './pages/userHome'
 import './App.css';
 import MoviePage from './pages/moviePage';
+=======
+
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Main from './pages/main';
+import LogIn from './pages/logIn';
+import UserHome from './pages/userHome'
+>>>>>>> 55030bc (adding home, movies and promises to the API)
 
 function Movy(){
   return(
@@ -20,9 +31,12 @@ function Movy(){
         <Route path="/home">
           <UserHome/>
         </Route>
+<<<<<<< HEAD
         <Route path="/movie/:id">
          <MoviePage/>
         </Route>
+=======
+>>>>>>> 55030bc (adding home, movies and promises to the API)
       </Switch>
     </Router>
     </>
--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
  export let getRandomNumber = function (max) {
     return Math.floor(Math.random() * max );
 }
+=======
+/* eslint-disable no-unreachable */
+ export let getRandomNumber = function (max) {
+    return Math.floor(Math.random() * max );
+}
+>>>>>>> 55030bc (adding home, movies and promises to the API)
