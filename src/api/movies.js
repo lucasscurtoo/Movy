081fa2fc93b6f1f55d34a5b07d0cc82c0b85bfc1
lucasscurtoo <lucascurtoo@gmail.com>
2021-08-24 17:55:21 -0300

    uploading changes

--- src/api/movies.js
+++ src/api/movies.js
@@ -27,5 +27,10 @@ export const getMovieDetails = (movieId) => {
   return fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`)
   .then((response) => response.json())
 }
+export const getMovieCredits = (movieId) => {   
+  return fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`)
+  .then((response) => response.json())
+}
+
 
 
--- src/pages/moviePage.jsx
+++ src/pages/moviePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import { useParams } from 'react-router';
-import { getMovieDetails,IMAGE_URL } from '../api/movies'; 
+import { getMovieCredits, getMovieDetails,IMAGE_URL } from '../api/movies'; 
+import playImage from '../images/playImg.jpg'
 import '../styles/moviePage.css'
 
 
@@ -8,6 +9,7 @@ function MoviePage(){
     let {id} = useParams();
 
     const [ movieRequested, setMovieRequested ] = useState(null);
+    const [ movieCredits, setMovieCredits ] = useState(null);
 
     useEffect(() => {
         getMovieDetails(id)
@@ -15,16 +17,47 @@ function MoviePage(){
             setMovieRequested(response);
         })
     }, [])
-    
+    useEffect(() => {
+        getMovieCredits(id)
+        .then((response) => {
+            setMovieCredits(response);
+        })
+    }, [])
+    console.log(movieCredits) 
+    console.log(movieRequested) 
+    let cast = movieCredits?.cast.slice(0,3)
+
  return(
       <>
          <div className="moviePage-box" style={{backgroundImage: `url(${IMAGE_URL}${movieRequested?.poster_path})`}}>
             <div className="movie-play">
                 <h1>{movieRequested?.title}</h1>
+                <button><img src={playImage}></img>Play</button>
             </div>
          </div>
          <div className="movie-info">
-
+             <section className="container">
+                 <ul>
+                     <li></li>
+                     <li></li>
+                     <li></li>
+                     <li></li>
+                 </ul>
+            <p>{movieRequested?.overview}</p>
+             </section>
+             <section className="container">
+                <ul>
+                    <li>Cast:&nbsp;</li>
+                    <li>{cast?.map((item) => `  ${item.name}, `)}</li>
+                    <li>more</li>
+                </ul>
+                <ul>
+                    <li>Genres:&nbsp;</li>
+                    {/* <li>{movieRequested?.map((item) => ` ${item.name}`)}</li> */}
+                    <li>more</li>
+                </ul>   
+                {/* <p>{movieRequested?.title}</p> */}
+             </section>
          </div>
          <div className="more-like-this">
 
