3fa7ad2c29137cf85be6d8389c1707e53914a506
lucasscurtoo <lucascurtoo@gmail.com>
2021-09-14 14:58:57 -0300

    adding redux to the project

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Carousel = ({data, name}) => {
-
     return (
         <div className="carousel-box w-scroll">
             <h1 className="text-3xl sticky left-0">{name}</h1>
