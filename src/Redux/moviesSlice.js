00f2c7df642294a4137d60b449339dde4ff21013
lucasscurtoo <lucascurtoo@gmail.com>
2021-09-22 14:47:49 -0300

    subiendo archivos para ver como arreglar un problema de redux

--- src/Redux/moviesSlice.js
+++ src/Redux/moviesSlice.js
@@ -1,57 +1,58 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getNowPlayingMovies, getPopularMovies, getUpcomingMovies } from '../api/movies';
+import { getMovieDetails, getNowPlayingMovies, getPopularMovies, getUpcomingMovies } from '../api/movies';
 
-export const fetchPopularMovies = createAsyncThunk('movies/fetchPopular', async () => {
+export const fetchPopularMovies = createAsyncThunk('movies/fetchPopular', async() => {
     const response = await getPopularMovies();
     return response.results;
 })
-export const fetchUpcomingMovies = createAsyncThunk('movies/fetchUpcoming', async () => {
+export const fetchUpcomingMovies = createAsyncThunk('movies/fetchUpcoming', async() => {
     const response = await getUpcomingMovies();
     return response.results;
 })
-export const fetchNowPlayingMovies = createAsyncThunk('movies/fetchNowPlaying', async () => {
+export const fetchNowPlayingMovies = createAsyncThunk('movies/fetchNowPlaying', async() => {
     const response = await getNowPlayingMovies();
     return response.results;
 })
+export const fetchMovieDetails = createAsyncThunk('movies/movieDetails', async(movieId) =>{
+    const response = await getMovieDetails(movieId);
+    return response.results;
 
-
+})
 export const moviesSlice = createSlice({
     name: 'movies',
-    initialState:{
+    initialState: {
         loading: false,
         popular: [],
         upcoming: [],
         nowPlaying: [],
         backgroundMovie: [],
+        movieDetails: [],
     },
-    reducers:{
-        setBackgroundMovie:(state,action) =>{
+    reducers: {
+        setBackgroundMovie: (state, action) => {
             state.backgroundMovie = action.payload
-        },
+        }
     },
     extraReducers: builder => {
-        builder
-            .addCase(fetchPopularMovies.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(fetchPopularMovies.fulfilled, (state, action) => {
-                state.loading = false;
-                state.popular = action.payload;
-            })
-            .addCase(fetchUpcomingMovies.pending, (state) =>{
-                state.loading = true;
-            })
-            .addCase(fetchUpcomingMovies.fulfilled, (state, action) => {
-                state.loading = false;
-                state.upcoming = action.payload;
-            })
-            .addCase(fetchNowPlayingMovies.pending, (state) =>{
-                state.loading = true;
-            })
-            .addCase(fetchNowPlayingMovies.fulfilled, (state,action) => {
-                state.loading = false;
-                state.nowPlaying = action.payload;
-            })
+        builder.addCase(fetchPopularMovies.pending, (state) => {
+            state.loading = true;
+        }).addCase(fetchPopularMovies.fulfilled, (state, action) => {
+            state.loading = false;
+            state.popular = action.payload;
+        }).addCase(fetchUpcomingMovies.pending, (state) => {
+            state.loading = true;
+        }).addCase(fetchUpcomingMovies.fulfilled, (state, action) => {
+            state.loading = false;
+            state.upcoming = action.payload;
+        }).addCase(fetchNowPlayingMovies.pending, (state) => {
+            state.loading = true;
+        }).addCase(fetchNowPlayingMovies.fulfilled, (state, action) => {
+            state.loading = false;
+            state.nowPlaying = action.payload;
+        })
+        .addCase(fetchMovieDetails.fulfilled, (state, action) =>{
+            state.movieDetails = action.payload;
+        })
     }
 })
 
--- src/pages/moviePage.jsx
+++ src/pages/moviePage.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router';
 <<<<<<< HEAD
 import { getMovieCredits, getMovieDetails,IMAGE_URL } from '../api/movies'; 
 import playImage from '../images/playImg.jpg'
+import backImage from '../images/flechaizq.png'
+import {Link} from 'react-router-dom';
 
 <<<<<<< HEAD
 =======
@@ -37,7 +39,11 @@ function MoviePage(){
 >>>>>>> fa7577d (onclick opens a page with the movie clicked)
 =======
     const [ movieCredits, setMovieCredits ] = useState(null);
+<<<<<<< HEAD
 >>>>>>> 081fa2f (uploading changes)
+=======
+   
+>>>>>>> 3cda24c (subiendo archivos para ver como arreglar un problema de redux)
 
     useEffect(() => {
         getMovieDetails(id)
@@ -57,6 +63,7 @@ function MoviePage(){
     }, [])
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
    
     const cast = movieCredits?.cast.slice(0,3)
 
@@ -68,6 +75,9 @@ function MoviePage(){
     console.log(movieRequested) 
     let cast = movieCredits?.cast.slice(0,3)
 =======
+=======
+  
+>>>>>>> 3cda24c (subiendo archivos para ver como arreglar un problema de redux)
    
     const cast = movieCredits?.cast.slice(0,3)
 >>>>>>> 7fcbc16 (making changes to merge)
@@ -76,6 +86,7 @@ function MoviePage(){
  return(
       <>
          <div className="moviePage-box" style={{backgroundImage: `url(${IMAGE_URL}${movieRequested?.poster_path})`}}>
+<<<<<<< HEAD
             <div className="movie-play">
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -107,6 +118,16 @@ function MoviePage(){
                 <h1 className="text-blue">{movieRequested?.title}</h1>
 >>>>>>> 17b391e (adding redux to the project)
                 <button><img src={playImage}></img>Play</button>
+=======
+              <div className="h-3/5 w-11/12 mt-2">
+                  <Link to="/home">
+                    <img className="w-8 " src={backImage}/>
+                  </Link>
+              </div>
+            <div className="w-11/12 h-2/5 ml-auto text-white">
+                <h1 className="text-blue">{movieRequested?.title}</h1>
+                <button className="py-2 px-4 bgc-white text-black font"><img className="w-10 inline" src={playImage}></img>Play</button>
+>>>>>>> 3cda24c (subiendo archivos para ver como arreglar un problema de redux)
             </div>
             
          </div>
