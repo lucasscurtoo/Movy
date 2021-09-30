4e220c5e1ca1b57747f9ee0a2f6cd38927a2896e
lucasscurtoo <lucascurtoo@gmail.com>
2021-09-30 15:17:11 -0300

    adding movieSimilars to the movie page

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -5,14 +5,15 @@ import {Link} from 'react-router-dom';
 import '../styles/carouselStyles.css';
 
 const Movie = ({ data }) => {
-
   return (
+
     <Link to={`movie/${data.id}`}  >
       <div className="movie ">
        <img src={`${IMAGE_URL}${data?.poster_path}`}/>
       </div>
     </Link>
   )
+
 }
 Movie.propTypes={
    data: PropTypes.array.isRequired,
