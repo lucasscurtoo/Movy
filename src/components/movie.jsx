d87c90a30eb6a69a1624d23872702aa0cccae98f
lucasscurtoo <lucascurtoo@gmail.com>
2021-09-30 15:17:11 -0300

    adding movieSimilars to the movie page

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 const Movie = ({ data }) => {
-
   return (
+
     <Link to={`movie/${data.id}`}  >
       <div className="w-52 h-5/6 ml-2 enlarge">
        <img  className="w-full h-auto object-cover" src={`${IMAGE_URL}${data?.poster_path}`}/>
       </div>
     </Link>
   )
+
 }
 Movie.propTypes={
    data: PropTypes.array.isRequired,
--- src/pages/moviePage.jsx
+++ src/pages/moviePage.jsx
@@ -96,7 +96,6 @@ const trailer = `${Youtube_URL}${movieVideos[0]?.key}?controls=0?&autoplay=1`
                     </div> 
                 </div>
             </div>
-
         </>
     )
 }
