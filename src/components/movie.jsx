09588040fb00b02ccec0682916eef8d29863c0e5
lucasscurtoo <lucascurtoo@gmail.com>
2021-10-05 13:20:19 -0300

    uploadingMoreFeatures

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,22 +1,18 @@
-
 import React from 'react';
 import { IMAGE_URL } from '../api/movies';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import '../styles/carouselStyles.css';
 
-
-
 const Movie = ({ data }) => {
-  return (
 
+  return (
     <Link to={`movie/${data.id}`}  >
-      <div className="movie">
+      <div className="movie ">
        <img src={`${IMAGE_URL}${data?.poster_path}`}/>
       </div>
     </Link>
   )
-
 }
 Movie.propTypes={
    data: PropTypes.array.isRequired,
--- src/components/movieSimilar.jsx
+++ src/components/movieSimilar.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import {IMAGE_URL} from '../api/movies';
+import { IMAGE_URL } from '../api/movies';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 
-const MovieSimilar = ({data}) => {
-    return(
-       <>
-       <div className="w-64 h-60">
-            <div className="w-full h-52 flex justify-end align-start p-2 bg-no-repeat bg-cover bg-top" style={{backgroundImage: `url(${IMAGE_URL}${data?.poster_path})`}} >
-                <h1 className="text-white font-bold">{data.release_date.slice(0,4)}</h1>
-            </div>
-            <div className="w-full h-20 bgGray">
-                <FontAwesomeIcon icon={faPlus} size="2x" className="ml-52 mt-2 cursor-pointer white-to-gray"/>
-            </div>
-            <div className="w-full h-60 bgGray">
-            <h1 className="text-white overflow-clip overflow-hidden ">{data.overview}</h1>
-            </div>
 
-       </div>
-       </>
+const MovieSimilar = ({ data }) => {
+
+    return (
+        <>
+            <Link to={`movie/${data.id}`}>
+                <div className="w-64 h-max mr-6 mt-6 bgGray moviesimilar">
+                    <div className="w-full h-40 flex justify-end align-start p-2 bg-no-repeat bg-cover bg-top rounded-t-lg" style={{ backgroundImage: `url(${IMAGE_URL}${data?.poster_path})` }} >
+                        <h1 className="text-white font-bold">{data.release_date.slice(0, 4)}</h1>
+                    </div>
+                    <div className="w-full h-12 bgGray flex">
+                        <h1 className="inline w-3/6 m-auto text-white truncate font-bold">{data?.title}</h1>
+                        <FontAwesomeIcon icon={faPlus} size="2x" className="w-3/6 ml-auto mr-2 mt-2 cursor-pointer white-to-gray" />
+                    </div>
+                    <div className="w-full h-48 bgGray rounded-b-lg ">
+                        <h1 className="text-white overflow-clip overflow-hidden h-full text-gray-300 w-11/12 mx-auto">{data.overview}</h1>
+                    </div>
+                </div>
+            </Link>
+        </>
     )
 }
 
-MovieSimilar.propTypes={
-    data:PropTypes.array.isRequired,
+MovieSimilar.propTypes = {
+    data: PropTypes.array.isRequired,
 }
 export default MovieSimilar;
