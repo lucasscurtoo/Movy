5295cef52d0269b19874899b6dafbf16dc85a31b
lucasscurtoo <lucascurtoo@gmail.com>
2021-11-03 16:33:40 -0300

    uploading auth0 feature

---
+++ src/auth/userProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = () => {
+    const { user, isAuthenticated } = useAuth0();
+
+    console.log(user)
+    return (
+      isAuthenticated && (
+        <div className="w-25 h-full flex  items-center">
+          <img className="w-8 rounded-2xl" src={user.picture} alt={user.name} />
+          <h2 className="mx-2">Profile</h2>
+        </div>
+      )
+    );
+  };
+  
+  export default Profile;
